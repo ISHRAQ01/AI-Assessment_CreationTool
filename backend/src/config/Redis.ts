720ac23c51569617693a28b7dd286f53334439cc
ISHRAQ01 <ishraq0641@gmail.com>
2026-05-26 19:54:13 +0530

    redis connection updated

--- backend/src/config/Redis.ts
+++ backend/src/config/Redis.ts
@@ -1,6 +1,10 @@
-import { Redis } from 'ioredis';
+import Redis from 'ioredis';
 
 const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
-const redisClient = new Redis(redisUrl);
+
+
+const redisClient = new Redis(redisUrl, {
+  maxRetriesPerRequest: null,
+});
 
 export default redisClient;
